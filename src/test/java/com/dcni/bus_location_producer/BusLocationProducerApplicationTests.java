4629ead36843b974166f28762d8400ce6d780950
BriamCarrasco <briamcarrasco.f@gmail.com>
2026-02-05 00:13:46 -0300

    Archivos iniciales

---
+++ src/test/java/com/dcni/bus_location_producer/BusLocationProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dcni.bus_location_producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusLocationProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
